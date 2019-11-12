447782b5e685a9d1857c3bc1ae2c92b434f108d3
efa2000 <efa2000@mail.ru>
2019-11-13 02:19:26 +0300

    nodes

--- alice.js
+++ alice.js
@@ -1,4 +1,4 @@
-const firebase = require('firebase');
+var firebase = require('firebase');
 const firebaseConfig = {
     apiKey: "AIzaSyDQUn6EhNOgSAV15do8DKwwx3KHDyvLGJc",
     authDomain: "node-red-alice-4462f.firebaseapp.com",
@@ -11,25 +11,105 @@ const firebaseConfig = {
   };
 
 module.exports = function(RED) {
-  //Config node, Alice-Service
+  //Sevice node, Alice-Service (credential)
   function AliceService(config) {
     RED.nodes.createNode(this,config);
-    var fb = firebase.initializeApp(firebaseConfig);
-    const email = this.credentials.username;
+    var fb;
+    try {
+      fb = firebase.initializeApp(firebaseConfig,this.id); 
+    } catch (error) {
+      if (error.code == 'app/duplicate-app'){
+        this.debug("Dublicated firebase app");
+        fb = firebase.app(this.id);
+      }else{
+        this.error(error);
+      }
+    }
+    const email = this.credentials.email;
     const password = this.credentials.password;
-    fb.auth().createUserWithEmailAndPassword(email, password)
+    fb.auth().signInWithEmailAndPassword(email, password)
+      .then(u=>{
+        this.emit("online");
+      })
       .catch(err=>{
         this.error(err.message);
       });
-    this.getDb = function(){
-      return fb.firestore();
-    }
-  };
+    
+    this.getRef = function(deviceid){
+      var user = fb.auth().currentUser;
+      return new Promise((resolve,reject)=>{
+        if (user){
+          resolve(fb.firestore().collection('users').doc(user.uid).collection('devices').doc(deviceid));
+        }else{
+          fb.auth().onAuthStateChanged(u=>{
+            if (u){
+              user = u;
+              resolve(fb.firestore().collection('users').doc(user.uid).collection('devices').doc(deviceid));
+            }
+          })
+        }
+      })
+    };
 
+    this.on('close', function(done) {
+      fb.delete().finally(r=>{
+        done();
+      });
+    });
+  };
   RED.nodes.registerType("alice-service",AliceService,{
     credentials: {
         email: {type:"text"},
         password: {type:"password"}
     }
   });
+  
+
+  function AliceDevice(config){
+    RED.nodes.createNode(this,config);
+    this.service = RED.nodes.getNode(config.service);
+    this.name = config.name;
+    this.description = config.description,
+    this.room = config.room,
+    this.service.getRef(this.id)
+      .then(ref=>{
+        this.deviceRef = ref;
+        return this.deviceRef
+      })
+      .then(ref=>{
+        ref.set({
+          name: config.name,
+          description: config.description,
+          room: config.room
+        })
+      });
+
+    this.service.on("online",()=>{
+      this.emit("online");
+    });
+
+    // this.on('close', (removed, done)=>{
+    //   if (removed){
+    //     console.log("dell start");
+    //     this.deviceRef.delete();
+    //     done();
+    //   }else{
+    //     done();
+    //   }
+    // });
+  };
+  RED.nodes.registerType("alice-device",AliceDevice);
+
+  function AliceOnOff(config){
+    RED.nodes.createNode(this,config);
+    this.status({fill:"red",shape:"dot",text:"offline"});
+    this.device = RED.nodes.getNode(config.device);
+    this.name = config.name;
+
+    this.device.on("online",()=>{
+      this.status({fill:"green",shape:"dot",text:"online"});
+    })
+  }  
+  RED.nodes.registerType("On_Off",AliceOnOff);
+
 };
