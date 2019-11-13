9e863120686e5f61d67925f6e05532cc7e16fb54
efa2000 <efa2000@mail.ru>
2019-11-13 19:11:04 +0300

    onoff

--- alice.js
+++ alice.js
@@ -15,6 +15,7 @@ module.exports = function(RED) {
   function AliceService(config) {
     RED.nodes.createNode(this,config);
     var fb;
+  
     try {
       fb = firebase.initializeApp(firebaseConfig,this.id); 
     } catch (error) {
@@ -25,37 +26,35 @@ module.exports = function(RED) {
         this.error(error);
       }
     }
+    
     const email = this.credentials.email;
     const password = this.credentials.password;
-    fb.auth().signInWithEmailAndPassword(email, password)
-      .then(u=>{
+    
+    this.authStateSubs =  fb.auth().onAuthStateChanged(u=>{
+      if (u){
         this.emit("online");
-      })
+      }else{
+        this.emit("offline");
+      }
+    });
+
+    fb.auth().signInWithEmailAndPassword(email, password)
       .catch(err=>{
         this.error(err.message);
       });
     
     this.getRef = function(deviceid){
       var user = fb.auth().currentUser;
-      return new Promise((resolve,reject)=>{
-        if (user){
-          resolve(fb.firestore().collection('users').doc(user.uid).collection('devices').doc(deviceid));
-        }else{
-          fb.auth().onAuthStateChanged(u=>{
-            if (u){
-              user = u;
-              resolve(fb.firestore().collection('users').doc(user.uid).collection('devices').doc(deviceid));
-            }
-          })
-        }
-      })
+      return fb.firestore().collection('users').doc(user.uid).collection('devices').doc(deviceid)
     };
 
-    this.on('close', function(done) {
-      fb.delete().finally(r=>{
-        done();
-      });
+    this.on('close',(done)=>{
+      setTimeout(()=>{
+        fb.auth().signOut();
+        fb.delete().finally(r=>{done()});
+      },500)
     });
+
   };
   RED.nodes.registerType("alice-service",AliceService,{
     credentials: {
@@ -70,45 +69,117 @@ module.exports = function(RED) {
     this.service = RED.nodes.getNode(config.service);
     this.name = config.name;
     this.description = config.description,
-    this.room = config.room,
-    this.service.getRef(this.id)
-      .then(ref=>{
-        this.deviceRef = ref;
-        return this.deviceRef
-      })
-      .then(ref=>{
-        ref.set({
-          name: config.name,
-          description: config.description,
-          room: config.room
-        })
+    this.room = config.room;
+    this.initState = false;
+    this.ref = null;
+
+    this.init = ()=>{
+      this.ref = this.service.getRef(this.id);
+      this.ref.set({
+        name: config.name,
+        description: config.description,
+        room: config.room
+      }).then(ref=>{
+        this.initState = true;
+        this.status({fill:"green",shape:"dot",text:"online"});
+        this.emit("online");
       });
-
+    }
+    
+    this.getRef=(capId)=>{
+      return this.ref.collection('capabilities').doc(capId);
+    }
+    
     this.service.on("online",()=>{
-      this.emit("online");
+      if (!this.initState){
+        this.init();
+      }else{
+        this.ref = this.service.getRef(this.id);
+        this.emit("online");
+      }
     });
+    this.service.on("offline",()=>{
+      this.emit("offline");
+      this.ref = null;
+      this.status({fill:"red",shape:"dot",text:"offline"});
+    })
 
-    // this.on('close', (removed, done)=>{
-    //   if (removed){
-    //     console.log("dell start");
-    //     this.deviceRef.delete();
-    //     done();
-    //   }else{
-    //     done();
-    //   }
-    // });
+    this.on('close', (removed, done)=>{
+      if (removed){
+        this.ref.delete();
+        done();
+      }else{
+        done();
+      }
+    });
   };
   RED.nodes.registerType("alice-device",AliceDevice);
 
   function AliceOnOff(config){
     RED.nodes.createNode(this,config);
-    this.status({fill:"red",shape:"dot",text:"offline"});
     this.device = RED.nodes.getNode(config.device);
     this.name = config.name;
+    this.initState = false;
+    this.ref = null;
+
+    this.init = ()=>{
+      this.ref = this.device.getRef(this.id);
+      let capab = {
+        type: "devices.capabilities.on_off",
+        retrievable: true,
+        parametrs: {
+          instance: "on",
+        },
+        state: {
+          value: false
+        },
+        action:{
+          value: false
+        }
+      };
+      this.ref.set(capab)
+        .then(ref=>{
+          this.status({fill:"green",shape:"dot",text:"online"});
+          this.initState = true;
+        });
+    };
 
     this.device.on("online",()=>{
-      this.status({fill:"green",shape:"dot",text:"online"});
+      if (!this.initState){
+        this.init();
+      }else{
+        this.ref = this.device.getRef(this.id);
+        this.status({fill:"green",shape:"dot",text:"online"});
+      }
+    });
+
+    this.device.on("offline",()=>{
+      this.ref = null;
+      this.status({fill:"red",shape:"dot",text:"offline"});
     })
+
+    this.on('input', (msg, send, done)=>{
+      if (typeof msg.payload != 'boolean'){
+        this.error("Wrong type! msg.payload must be boolean.");
+        if (done) {done();}
+        return;
+      }
+      if (!this.ref){
+        this.error("Device offline");
+        this.status({fill:"red",shape:"dot",text:"offline"});
+        if (done) {done();}
+        return;
+      };      
+    })
+
+    this.on('close', (removed, done)=>{
+      if (removed){
+        this.ref.delete();
+        done();
+      }else{
+        done();
+      }
+    });
   }  
   RED.nodes.registerType("On_Off",AliceOnOff);
 
